8fb3767bcfe2f273aff638be7d3f4f03613e24e3
kurtcoke <pritzel@gmail.com>
2015-10-22 13:59:54 +0200

    Added another unit test, more coming soon

--- src/srnd/srnd_test.go
+++ src/srnd/srnd_test.go
@@ -1,6 +1,7 @@
 package srnd
 
 import "testing"
+import "bytes"
 
 func TestGenFeedsConfig(t *testing.T) {
 
@@ -33,3 +34,68 @@ func TestOpenFileWriter(t *testing.T) {
 	}
 
 }
+
+
+
+//store := createArticleStore(conf.store, nil)
+
+//files, err := store.GetAllAttachments()
+
+
+/*
+
+func TestGetThreadsPerPage(t *testing.T) {
+
+
+
+nntp := createNNTPConnection()
+group := nntp.Newsgroup()
+
+//ppb, err := self.database.GetPagesPerBoard(group)
+
+_, err := self.database.GetThreadsPerPage(group)
+	// Generate default feeds.ini
+	if err != nil {
+
+		t.Error("Cant get threads per page.", err)
+
+	}
+
+
+}
+
+*/
+
+func TestRenderTo(t *testing.T) {
+
+	c := new(boardModel)
+
+	var b bytes.Buffer
+
+	err := c.RenderTo(&b)
+	// Generate default feeds.ini
+	if err != nil {
+
+		t.Error("Error rendering bytes.", err)
+
+	}
+
+}
+
+
+ //(self boardModel) RenderTo(wr io.Writer) error {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
